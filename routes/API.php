ef9d13b19bbac73422080b27c9c72f632d9dce28
NghiaBlack <ntn16092003@gmail.com>
2025-03-28 18:55:45 +0700

    FEAT - Modify Note

--- routes/API.php
+++ routes/API.php
@@ -4,6 +4,7 @@ use WorkSpace\Controller\AuthController;
 use WorkSpace\Controller\SystemController;
 use WorkSpace\Controller\WorkSpaceController;
 use WorkSpace\Controller\ProjectController;
+use WorkSpace\Controller\NoteController;
 use Middleware\Authenticate;
 
 return function (Router $router) {
@@ -36,5 +37,11 @@ return function (Router $router) {
 
    $router->group(['prefix' => 'project', 'middleware' => 'auth'], function (Router $router) {
       $router->post('/add', [ProjectController::class, 'createNewProject']);
+   });
+   
+   //--------------------------------------------------PROJECT--------------------------------------------------//
+
+   $router->group(['prefix' => 'note', 'middleware' => 'auth'], function (Router $router) {
+      $router->put('/modify/{IDNote}', [NoteController::class, 'ModifyNote']);
    });
 };
